41ffdf0662a8737be4f4d8b1b9b06012f9062857
Maryna Dukhnich <maryna.dukhnich@gmail.com>
2023-11-06 18:53:54 +0100

    change title

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,21 +3,26 @@ import { MenuItem } from '../../components/MenuItem';
 import './style.css';
 
 export const HomePage = () => {
-  const [menuOpened, setMenuOpened] = useState(false);
-  const toggleButton = () => setMenuOpened(!menuOpened);
   const links = ["Domů", "Naše nabídka", "Náš tým", "Blog", "Kontakt"];
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [pageTitle, setPageTitle] = useState(links[0]);
+  const toggleButton = () => setMenuOpened(!menuOpened);
+  const handleSelect = (text) => {
+    setPageTitle(text);
+    toggleButton();
+  }
   return (
     <>
       <header>
         <div className={menuOpened ? "menu" : "menu menu--closed"}>
           <button className="menu__btn" onClick={toggleButton}></button>
           <div className="menu__items">
-            {links.map(text => <MenuItem text={text} key={text} onSelect={toggleButton}/>)}
+            {links.map(text => <MenuItem text={text} key={text} onSelect={handleSelect}/>)}
           </div>
         </div>
       </header>
       <main>
-        <h1>Domů</h1>
+        <h1>{pageTitle}</h1>
       </main>
     </>
   );
